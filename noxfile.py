6eff0c78a7516d5304604471c7a8825568271df4
koviubi56 <koviubi56@duck.com>
2023-10-22 17:12:15 +0200

    chore: :hammer: Make noxfile.py better
    Removed unnecessary things, added tests for 3.12 and 3.13, ...
    
    Signed-off-by: koviubi56 <koviubi56@duck.com>

--- noxfile.py
+++ noxfile.py
@@ -1,30 +1,17 @@
 import os
 import sys
-from typing import Any, Callable, Mapping, TypeVar
 
-import dotenv
 import nox
 
 # By default (unless explicit set otherwise) running now should only
 # run `test`, because `test_coverage` also uploads to CodeCov
 nox.options.sessions = ["test"]
 
-T = TypeVar("T")
-PYTHON_VERSIONS = ["3.8", "3.9", "3.10", "3.11"]
-
-
-def get_or_else(
-    mapping: Mapping[T, Any], key: T, or_else: Callable[[], T]
-) -> T:
-    try:
-        return mapping[key]
-    except KeyError:
-        return or_else()
+PYTHON_VERSIONS = ["3.8", "3.9", "3.10", "3.11", "3.12", "3.13"]
 
 
 @nox.session(python=PYTHON_VERSIONS)
 def test_coverage(session: nox.Session) -> None:
-    dotenv.load_dotenv()
     session.install("-U", "pip", "setuptools", "wheel")
     session.install(
         "-U", "-r", "requirements.txt", "pytest-randomly", "coverage"
@@ -39,20 +26,21 @@ def test_coverage(session: nox.Session) -> None:
         "-vv",
         "-r",
         "A",
-        "-l",
+        # "-l",
         "--color=yes",
         "--code-highlight=yes",
         "--continue-on-collection-errors",
     )
-    if os.name != "nt":
-        session.run(
-            "curl",
-            "-O",
-            "-s",
-            "https://uploader.codecov.io/latest/linux/codecov",
-        )
-        session.run("chmod", "+x", "codecov")
-        session.run("./codecov", env={"CODECOV_TOKEN": codecov_token})
+    if os.name == "nt":
+        raise RuntimeError("cannot upload coverage results on Windows")
+    session.run(
+        "curl",
+        "-O",
+        "-s",
+        "https://uploader.codecov.io/latest/linux/codecov",
+    )
+    session.run("chmod", "+x", "codecov")
+    session.run("./codecov", env={"CODECOV_TOKEN": codecov_token})
 
 
 @nox.session(python=PYTHON_VERSIONS)
@@ -69,7 +57,7 @@ def test(session: nox.Session) -> None:
         "-vv",
         "-r",
         "A",
-        "-l",
+        # "-l",
         "--color=yes",
         "--code-highlight=yes",
         "--continue-on-collection-errors",
